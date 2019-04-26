9ad97fe1502ea730f75e35f520967214767e0682
Anna <annas.wieclaw@gmail.com>
2019-04-26 16:13:15 +0100

    Initial commit on personal account

---
+++ src/WhatWillThisClassDo.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author awieclaw
+ *
+ */
+
+
+public class WhatWillThisClassDo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("First\n" +"time java runs");
+	}
+
+}
