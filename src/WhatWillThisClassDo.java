1758514bb47e8717dbd281abe6507931ba87d18a
Anna <annas.wieclaw@gmail.com>
2019-04-27 23:47:21 +0100

    Second commit

--- src/WhatWillThisClassDo.java
+++ src/WhatWillThisClassDo.java
@@ -10,7 +10,7 @@ public class WhatWillThisClassDo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("First\n" +"time java runs");
-//TODO check
+//TODO check HELLO :)
 	}
 
 }
